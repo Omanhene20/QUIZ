002fa04b863bf116e5b2f4eaea125dfd79c60fe5
Omanhene20 <144060132+Omanhene20@users.noreply.github.com>
2023-12-27 02:33:34 -0600

    I was tasked to create a quiz for students from the scratch
    General chemistry quiz questions for students enrolled in the class

---
+++ question.js
@@ -0,0 +1,82 @@
+var questions = [
+    {
+        "question": "What is the chemical symbol for gold?",
+        "option1": "Au",
+        "option2": "Ag",
+        "option3": "Fe",
+        "option4": "Hg",
+        "answer": "1"
+    },
+    {
+        "question": "Which of the following elements is a noble gas?",
+        "option1": "Oxygen",
+        "option2": "Helium",
+        "option3": "Sodium",
+        "option4": "Flourine",
+        "answer": "2"
+    },
+    {
+        "question": "What is the atomic number of carbon?",
+        "option1": "6",
+        "option2": "12",
+        "option3": "16",
+        "option4": "22",
+        "answer": "1"
+    },
+    {
+        "question": "In a chemical equation, what do the coefficients represent?",
+        "option1": "Number of atoms",
+        "option2": "State of matter",
+        "option3": "Molar mass",
+        "option4": "Number of molecules",
+        "answer": "4"
+    },
+    {
+        "question": "Which of the following is a strong acid?",
+        "option1": "Acetic acid",
+        "option2": "Hydrochloric acid",
+        "option3": "Citric acid",
+        "option4": "Sulfuric acid",
+        "answer": "4"
+    },
+    {
+        "question": "What is the formula for methane?",
+        "option1": "CH4",
+        "option2": "CO2",
+        "option3": "H2O",
+        "option4": "C6H12O6",
+        "answer": "1"
+    },
+    {
+        "question": "Which subatomic particle is responsible for the chemical properties of an element?",
+        "option1": "Proton",
+        "option2": "Neutron",
+        "option3": "Electron",
+        "option4": "Nucleus",
+        "answer": "3"
+    },
+    {
+        "question": "What is the pH of a neutral solution?",
+        "option1": "0",
+        "option2": "7",
+        "option3": "14",
+        "option4": "1",
+        "answer": "2"
+    },
+    {
+        "question": "What is the process by which a solid changes directly to a gas without passing through the liquid state?",
+        "option1": "Sublimation",
+        "option2": "Evaporation",
+        "option3": "Condensation",
+        "option4": "Fusion",
+        "answer": "1"
+    },
+    {
+        "question": "Which law states that the total mass of substances involved in a chemical reaction remains constant?",
+        "option1": "Boyle's Law",
+        "option2": "Avogadro's Law",
+        "option3": "Law of Conservation of Mass",
+        "option4": "Charles's Law",
+        "answer": "3"
+    }
+];
